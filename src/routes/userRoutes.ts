553239751252917142c6b26b1f6f729f8b7f0534
Nemitor <pavel999977@gmail.com>
2024-09-08 22:28:42 +0500

    Prod

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 // src/routes/userRoutes.ts
 import express from 'express';
-import { getRoot,getProof } from '../controllers/userController';
+import {getRoot, getProof, getMessage} from '../controllers/userController';
 
 const router = express.Router();
 
 router.get('/merkle/root', getRoot);
 router.get('/merkle/proof/:id', getProof);
+router.get('/merkle/message/:id',getMessage)
 
 export default router;
